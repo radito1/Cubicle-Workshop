a037288016cfe435422d62ad2d74048eef483a86
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-05-30 15:42:11 +0300

    setup mongoose

---
+++ src/config/dbConfig.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const uri = 'monogodb://127.0.0.1:27017/cubicle';
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+};
+
+module.exports = dbConnect;
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./router');
 
 const app = express();
 
 const PORT = 3000;
+
 //Алтернативен вариянт
 expressConfig(app);                      //require('./config/expressConfig')(app);
 handlebarsConfig(app);                   //require('./config/hhandlebarsConfig')(app);
+
+dbConnect()
+    .then(() => console.log('DB connected successfully!'))
+    .catch(err => console.log('DB error:', err));
 
 app.use(routes);
 
